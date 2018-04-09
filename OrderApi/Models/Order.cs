fa8d39be93623f20b4624dadaa764fbc34cd3b4f
MartinDataStuff <martin1-g@hotmail.com>
2018-04-09 22:15:42 +0200

    Added CustomerApi service and added properties to the Product and Order models to better reflect the given model.

--- OrderApi/Models/Order.cs
+++ OrderApi/Models/Order.cs
@@ -5,7 +5,13 @@ namespace OrderApi.Models
     {
         public int Id { get; set; }
         public DateTime? Date { get; set; }
+        public OrderStaus Staus { get; set; }
+        public decimal ShippedCharged { get; set; }
+        public DateTime EstimatedDeliveryTime { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
+
+
+        public enum OrderStaus { Requested, Shipped, Completed, Cancelled, Paid }
     }
 }
--- ProductApi/Models/Product.cs
+++ ProductApi/Models/Product.cs
@@ -3,8 +3,10 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
         public decimal Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
         public int ItemsInStock { get; set; }
     }
 }
