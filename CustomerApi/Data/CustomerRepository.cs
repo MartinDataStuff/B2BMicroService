6c09e86146fed476d25780b7577e3c51bac5f2c2
MartinDataStuff <martin1-g@hotmail.com>
2018-04-12 11:31:51 +0200

    Added some repository related files in the Data folder for Customer

---
+++ CustomerApi/Data/CustomerRepository.cs
@@ -0,0 +1,42 @@
+﻿using CustomerApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApi.Data
+{
+    public class CustomerRepository : IRepository<Customer>
+    {
+        private readonly CustomerApiContext db;
+
+        public CustomerRepository(CustomerApiContext context)
+        {
+            db = context;
+        }
+        public Customer Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomerApi/Data/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using CustomerApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApi.Data
+{
+    public class DbInitializer
+    {
+        // This method will create and seed the database.
+        public static void Initialize(CustomerApiContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            // Look for any Products
+            if (context.Customers.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { CompanyName = "CompanyOne", Email = "example@email.com", BillingAddress = "Home" }
+            };
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+        }
+    }
+}
